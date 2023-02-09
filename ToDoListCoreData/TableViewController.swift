db5a38faef03857c34a0a76e56330503f251ce01
Matvei Bykadorov <2kydepsta2@gmail.com>
2023-02-09 22:30:40 +0300

    fetch and delete objects

--- ToDoListCoreData/TableViewController.swift
+++ ToDoListCoreData/TableViewController.swift
@@ -38,6 +38,21 @@ class TableViewController: UITableViewController {
         
         do {
             try context.save()
+            tasks.append(taskObj)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let fetchRequest = Task.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        do {
+            tasks = try context.fetch(fetchRequest)
         } catch let error {
             print(error)
         }
@@ -45,12 +60,17 @@ class TableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+//        if let objects = try? context.fetch(Task.fetchRequest()) {
+//            for object in objects {
+//                context.delete(object)
+//            }
+//        }
+//        do {
+//            try context.save()
+//        } catch let error {
+//            print(error)
+//        }
     }
 
     // MARK: - Table view data source
@@ -74,51 +94,5 @@ class TableViewController: UITableViewController {
         cell.contentConfiguration = content
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
